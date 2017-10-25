f0ece0c285e6110429f1e5e380cd019bc920fd07
WilliamKluge <klugewilliam@gmail.com>
2017-10-25 16:32:07 -0400

    Processing with grip, but still no results

--- src/main.py
+++ src/main.py
@@ -3,35 +3,37 @@
 # Date: 2017-10-25
 import cv2
 import numpy
+import time
 from moviepy.video.io.VideoFileClip import VideoFileClip
 from matplotlib import pyplot as plt
 
 from src.grip import GripPipeline
 
 
-def convert_image(img):
-    # img[:, :, 0] = numpy.ones([1080, 1920]) * 255
-    # img[:, :, 1] = numpy.ones([1080, 1920]) * 255
-    # img[:, :, 2] = numpy.ones([1080, 1920]) * 0
-    # 
-    # r, g, b = cv2.split(img)
-    # return cv2.merge([b, g, r])
-    return img[0, :, 0].max()
-
-
 def main():
-    pipline = GripPipeline
+    name_image = cv2.imread("name.png")
+    pipline = GripPipeline(name_image)
     input_path = input("Enter the path of the video to edit: ")
 
     input_video = VideoFileClip(input_path)
+
+    start = time.time()
+    processed_frames = 0
 
     for frame in input_video.iter_frames():
 
         pipline.process(frame)
 
         if pipline.find_contours_output:
-            # If the array is not none
+            # If the array is not none (there are contours
+            plt.subplot(111), plt.imshow(frame, cmap='gray')
+            plt.title("Contours found on image"), plt.xticks([]), plt.yticks([])
+            plt.suptitle("Scanned image")
+            plt.show()
 
+        processed_frames += 1
+        if processed_frames % 100 == 0:
+            print(processed_frames / (time.time() - start), "fps")
 
     print("Done.")
 
