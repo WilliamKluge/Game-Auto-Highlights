aa554f4bcf24abf6abc4f90842a10700caee2805
WilliamKluge <klugewilliam@gmail.com>
2017-11-07 14:44:27 -0500

    Clip selection and video output working, but selecting same clip multiple times

--- src/main.py
+++ src/main.py
@@ -4,6 +4,8 @@
 import cv2
 import numpy
 import time
+
+from moviepy.video.compositing.concatenate import concatenate_videoclips
 from moviepy.video.io.VideoFileClip import VideoFileClip
 from matplotlib import pyplot as plt
 
@@ -21,6 +23,10 @@ def print_stats(fps, frame_count):
 
 
 def main():
+    # Show every selected frame
+    FRAME_BY_FRAME = False
+    # Time (in seconds) to select around each kill
+    CLIP_SURROUND_TIME = 4
     # Image that holds the user's name
     name_image = cv2.imread("name.png")
     # Create the GRIP Pipeline for image processing
@@ -34,22 +40,51 @@ def main():
     start = time.time()
     # Amount of frames processed
     processed_frames = 0
+    # Video FPS
+    video_fps = input_video.fps
     # Amount of frames in the video
-    frame_count = int(input_video.fps * input_video.duration)
+    frame_count = int(video_fps * input_video.duration)
+    # Clips that were found to fit the criteria
+    selected_clips = []
+
+    last_clip_end = -1
 
     for frame in input_video.iter_frames():
         # Iterates through the frames in a clip
 
+        if processed_frames / video_fps < last_clip_end:
+            processed_frames += 1
+            continue
+        else:
+            last_clip_end = -1
+
         pipline.process(frame)  # Processes the frame using the GRIP filter
 
         if len(pipline.find_contours_output) > 0:
-            # If the array is not none (there are contours), show the associated images
-            plt.subplot(121), plt.imshow(frame, cmap='gray')
-            plt.title("Scanned Image"), plt.xticks([]), plt.yticks([])
-            plt.subplot(122), plt.imshow(pipline.rgb_threshold_output, cmap='gray')
-            plt.title("RGB Filter Output"), plt.xticks([]), plt.yticks([])
-            plt.suptitle("Frame Analysis")
-            plt.show()
+            # If the array is not none (there are contours)
+            if FRAME_BY_FRAME:
+                # show the associated images
+                plt.subplot(121), plt.imshow(frame, cmap='gray')
+                plt.title("Scanned Image"), plt.xticks([]), plt.yticks([])
+                plt.subplot(122), plt.imshow(pipline.rgb_threshold_output, cmap='gray')
+                plt.title("RGB Filter Output"), plt.xticks([]), plt.yticks([])
+                plt.suptitle("Frame Analysis")
+                plt.show()
+
+            clip_start_time = processed_frames / video_fps - CLIP_SURROUND_TIME
+            clip_end_time = processed_frames / video_fps + CLIP_SURROUND_TIME
+
+            if clip_start_time < 0:
+                clip_start_time = 1
+
+            if clip_end_time > input_video.duration:
+                # If the end of the clip would be past the end of the video
+                clip_end_time = input_video.duration
+
+            print("Selected video between", clip_start_time, "and", clip_end_time)
+
+            selected_clips.append(input_video.subclip(clip_start_time, clip_end_time))
+            last_clip_end = clip_end_time
 
         # Prints out stats
         processed_frames += 1
@@ -57,6 +92,10 @@ def main():
         if processed_frames % 10 == 0:
             fps = processed_frames / (time.time() - start)
             print_stats(fps, frame_count)
+
+    # Writing video file
+    final_clip = concatenate_videoclips(selected_clips)
+    final_clip.write_videofile(input_path[:4] + "_highlights.mp4")
 
     print("Done.")
 
